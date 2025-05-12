893efd06af0d96afed1d12fdcd2341a292cc3c1b
Guilherme Pinto <109026846+gsilvaepinto@users.noreply.github.com>
2025-05-12 15:23:06 +0100

    Add files via upload

---
+++ 1_TwoSum/app.js
@@ -0,0 +1,15 @@
+const twoSum = (nums, target) => {
+    const groupNums = [];
+
+    for (let i = 0; i < nums.length; i++){
+        for (let j = 0; j < nums.length; j++){
+            if (nums[i] + nums[j] === target){
+                groupNums.push([i, j]);
+            }
+        }
+    }
+
+    return groupNums;
+}
+
+console.log(twoSum([1, 1, 1, 1], 2));
